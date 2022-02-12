b2536f981e1d9c42032e6800888d914c012ebf0f
Noel <noel.huberto@gmail.com>
2022-02-12 11:39:17 +0100

    wip connexion form

--- src/main/java/com/noelh/paymybuddy/controller/FrontController.java
+++ src/main/java/com/noelh/paymybuddy/controller/FrontController.java
@@ -1,12 +1,13 @@
 package com.noelh.paymybuddy.controller;
 
+import com.noelh.paymybuddy.dto.SignInDTO;
 import com.noelh.paymybuddy.service.FrontService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.time.format.DateTimeFormatter;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FrontController {
@@ -14,21 +15,36 @@ public class FrontController {
     @Autowired
     private FrontService frontService;
 
-    @GetMapping
-    public String getIndex(){
-        return "Index";
-    }
-
-    @GetMapping("/HomePage")
-    public String getHomePage(Model model){
-        model.addAttribute("baseInfo", frontService.getUserAccountMinimalInfoById(1));
-        model.addAttribute("transaction", frontService.getMoneyTransactionListById(1));
-        return "HomePage";
-    }
+//    @GetMapping("/HomePage")
+//    public String getHomePage(Model model){
+//        model.addAttribute("baseInfo", frontService.getUserAccountMinimalInfoById(1));
+//        model.addAttribute("transaction", frontService.getMoneyTransactionListById(1));
+//        return "HomePage";
+//    }
 
     @GetMapping("/UserTransferPage")
     public String getUserTransferPage(Model model){
         model.addAttribute("loginMail","abcd@abcd.abcd");
         return "UserTransferPage";
+    }
+
+    @GetMapping()
+    public String getSignInPage(Model model){
+        SignInDTO signInDTO = new SignInDTO();
+        model.addAttribute("signInDTO",signInDTO);
+        return "SignInPage";
+    }
+
+    @PostMapping()
+    public String submitSignInPage(@ModelAttribute("signInDTO") SignInDTO signInDTO, Model model){
+        model.addAttribute("baseInfo", frontService.getUserAccountMinimalInfoById(1));
+        model.addAttribute("transaction", frontService.getMoneyTransactionListById(1));
+        System.out.println(signInDTO);
+        return "HomePage";
+    }
+
+    @GetMapping("SignUpPage")
+    public String getSignUpPage(){
+        return "SignUpPage";
     }
 }
