29183e754d4f3f814ff936c8717da7fe6cd23c9f
Noel <noel.huberto@gmail.com>
2022-03-23 12:35:59 +0100

    Javadoc added for model package

--- src/main/java/com/noelh/paymybuddy/model/MoneyTransactionWithUserAccount.java
+++ src/main/java/com/noelh/paymybuddy/model/MoneyTransactionWithUserAccount.java
@@ -5,27 +5,48 @@ import lombok.Data;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
+/**
+ * MoneyTransactionWithUserAccount model
+ */
 @Entity
 @Data
 public class MoneyTransactionWithUserAccount {
 
+    /**
+     * id for the MoneyTransactionWithUserAccount
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * date for the MoneyTransactionWithUserAccount
+     */
     @Column(nullable = false)
     private LocalDateTime date;
 
+    /**
+     * amount for the MoneyTransactionWithUserAccount
+     */
     @Column(nullable = false)
     private Double amount;
 
+    /**
+     * tax amount for the MoneyTransactionWithUserAccount
+     */
     @Column(nullable = false)
     private Double taxAmount;
 
+    /**
+     * sender for the MoneyTransactionWithUserAccount
+     */
     @ManyToOne
     @JoinColumn(nullable = false)
     private UserAccount sender;
 
+    /**
+     * receiver for the MoneyTransactionWithUserAccount
+     */
     @ManyToOne
     @JoinColumn(nullable = false)
     private UserAccount receiver;
--- src/main/java/com/noelh/paymybuddy/model/UserAccount.java
+++ src/main/java/com/noelh/paymybuddy/model/UserAccount.java
@@ -5,33 +5,60 @@ import lombok.Data;
 import javax.persistence.*;
 import java.util.List;
 
+/**
+ * UserAccount model
+ */
 @Entity
 @Data
 public class UserAccount {
 
+    /**
+     * id for the UserAccount
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * loginMail for the UserAccount
+     */
     @Column(nullable = false,
             unique = true)
     private String loginMail;
 
+    /**
+     * password for the UserAccount
+     */
     @Column(nullable = false)
     private String password;
 
+    /**
+     * balance for the UserAccount
+     */
     @Column(nullable = false)
     private Double balance;
 
+    /**
+     * friendList for the UserAccount
+     */
     @ManyToMany
     private List<UserAccount> friendList;
 
+    /**
+     * bankAccountList for the UserAccount
+     */
     @OneToMany
     private List<BankAccount> bankAccountList;
 
+    /**
+     * moneyTransactionWithBankAccountList for the UserAccount
+     */
     @OneToMany
     private List<MoneyTransactionWithBankAccount> moneyTransactionWithBankAccountList;
 
+    /**
+     * moneyTransactionWithUserAccountList for the UserAccount
+     */
     @ManyToMany
     private List<MoneyTransactionWithUserAccount> moneyTransactionWithUserAccountList;
 }
